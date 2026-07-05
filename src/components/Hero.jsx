69fa056d808167c170c6a721c082f76f3b507e55
Srushti <agrawalsp_2@rknec.edu>
2026-07-05 23:54:26 +0530

    Initial React project setup with Tailwind CSS

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+  return (
+    <section className="h-screen flex items-center justify-center">
+      <h1 className="text-7xl font-bold text-red-800">
+        Rahul The Beauty Concept
+      </h1>
+    </section>
+  );
+}
+
+export default Hero;
