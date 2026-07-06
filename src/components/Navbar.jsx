0bc2bd48040571195efac180ce3715490a084842
Srushti <agrawalsp_2@rknec.edu>
2026-07-06 20:22:39 +0530

    Initial React project setup with Tailwind CSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,29 @@
 function Navbar() {
   return (
     <nav>
-      Navbar
+
+      <div>
+
+        <h2>Rahul</h2>
+
+        <p>The Beauty Concept</p>
+
+      </div>
+
+      <ul>
+
+        <li>Home</li>
+
+        <li>Our World</li>
+
+        <li>Story</li>
+
+        <li>Visit</li>
+
+        <li>Instagram</li>
+
+      </ul>
+
     </nav>
   );
 }
